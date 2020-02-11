b29339cc3764244246eb89629aa3def2b75fecbd
Wei Zhang <wei.zhang4@outlook.com>
2020-02-11 13:46:41 -0800

    Implemented an input validator method in the StartScreenMenu Class.

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SpaceGame;
 
 namespace SpaceGame
 {
@@ -6,14 +7,16 @@ namespace SpaceGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Space Game!");
-            Character playerCharacter = new Character("Luis", 3);
-            Console.WriteLine($"Name:{playerCharacter.GetName()}");
-            Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
-            playerCharacter.AddCurrency(15);
-            Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
-            playerCharacter.SubtractCurrency(7);
-            Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
+            //Console.WriteLine("Space Game!");
+            //Character playerCharacter = new Character("Luis", 3);
+            //Console.WriteLine($"Name:{playerCharacter.GetName()}");
+            //Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
+            //playerCharacter.AddCurrency(15);
+            //Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
+            //playerCharacter.SubtractCurrency(7);
+            //Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
+            StartScreenMenu.Choices();
+
         }
         
     }
