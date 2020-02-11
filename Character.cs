8b269a0ba15342cdd727f68e58e3a01477c5ce27
Wei Zhang <wei.zhang4@outlook.com>
2020-02-11 13:21:12 -0800

    Initial Commit, refactored classes from the Program.cs. Renamed Program.cs to Main.cs. Created class for start screen menu.

---
+++ Character.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SpaceGame
+{
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public class Character
+    {
+        int age = 18, race = 0, currency = 0;
+        string charName = "";
+        public Character()
+        {
+            age = 18;
+            race = 1;
+            currency = 15;
+            charName = "Jimbo";
+        }
+
+        public Character(string cName, int raceNum)
+        {
+            age = 18;
+            race = raceNum;
+            charName = cName;
+            currency = 15;
+        }
+
+        public string GetName()
+        {
+            return charName;
+        }
+        public int GetCurrency()
+        {
+            return currency;
+        }
+        public void AddCurrency(int numToAdd)
+        {
+            currency += numToAdd;
+        }
+        public void SubtractCurrency(int numToSubtract)
+        {
+            currency -= numToSubtract;
+        }
+    }
+}
---
+++ StartScreenMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SpaceGame;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class StartScreenMenu
+{
+	public void Choices()
+	{
+        Console.WriteLine("Welcome to the Space Game!");
+        Console.WriteLine("Make a choice!");
+		
+
+	}
+}
