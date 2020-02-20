997341316b45aa46a55febc04158c1fadeba40de
Daniel Lungaro <chunk_77@hotmail.com>
2020-02-20 09:39:10 -0800

    updates

--- Program.cs
+++ Program.cs
@@ -7,8 +7,13 @@ namespace SpaceGame
         static void Main(string[] args)
         {
             Console.WriteLine("Space Game!");
+
             Character playerCharacter = new Character("Luis", 3);
+            Character defaultCharacter = new Character();
+
             Console.WriteLine($"Name:{playerCharacter.GetName()}");
+            Console.WriteLine($"Default Char Name:{defaultCharacter.GetName()}");
+
             Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
             playerCharacter.AddCurrency(15);
             Console.WriteLine($"Current Currency:{playerCharacter.GetCurrency()}");
@@ -54,6 +59,7 @@ namespace SpaceGame
             {
                 currency -= numToSubtract;
             }
+            
         }
     }
 }
